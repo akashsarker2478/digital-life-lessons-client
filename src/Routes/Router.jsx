8e26aba2504a0b1b1ad5b64a9ad9039f1646e30b
Akash Sarker <akashsarker2478@gmail.com>
2025-12-06 20:32:26 +0600

    login page add

--- src/Routes/Router.jsx
+++ src/Routes/Router.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router";
 import HomeLayouts from "../Layouts/Home Layout/HomeLayouts";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login page/Login";
 
 export const router = createBrowserRouter([
     {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
         {
             index:true,
             element:<Home></Home>   
+       },
+       {
+        path:'/login',
+        element:<Login></Login>
        }
     ]
     }
